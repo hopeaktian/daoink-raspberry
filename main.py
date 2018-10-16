7a325aef56b9450d89b3de4f8c282c7719da56c7
hopeaktian <hopeaktian@gmail.com>
2018-10-16 23:30:56 +0800

    debug queue

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ description:
 
 from config import engine, Session
 from model import Base, Order
-import datetime, traceback, time, requests
+import datetime, traceback, time, requests, subprocess
 
 
 
@@ -45,40 +45,35 @@ def Query():
                         f.write(str(datetime.datetime.now()) + " " + All_Order[i].File_Dir + " " + "success-download" + "\n")
                     # 在数据库中做出标记，文件已下载成功
                     All_Order[i].Print_Status = 2
-                    print "Ok"
                 finally:
                     session.commit()
-                    # session.close()
-                    # session.flush()
-                    print 'close'
-    print 'finished'
 
 def Print():
 
     Base.metadata.create_all(engine)
-    session = Session()
+    session2 = Session()
     # 将 ./User_Files/To_Print/ 中的文件名导入到预打印日志中
     cmd = "ls ./User_Files/To_Print/ > ./log/ToPrint_filename"
-    subprocess.call(cmd)
+    print cmd
+    subprocess.call(cmd, shell=True)
     ToPrint = open("./log/ToPrint_filename", 'r+')
     for line in ToPrint:
         try:
             # 开始尝试打印
             os.system('lpr ./User_Files/To_Print/{}' .format(line))
-        except Exception:
+        except Exception as e:
             # 捕获错误，并将错误写入错误日志中
-            s = traceback.format_exc()
             with open('./log/print_error_log', 'a') as f:
-                f.write(str(datetime.datetime.now()) + " " + s + "\n")
-        finally:
+                f.write(str(datetime.datetime.now()) + " " + str(e) + "\n")
+        else:
             # 将打印成功的文件移动到 ./User_Files/Finished_Print 这个目录中
-            os.system('mv ./User_Files/To_Print/{} ./User_Files/Finished_Print' .format(line))
+            os.system('mv ./User_Files/To_Print/{} ./User_Files/Finished_Print/' .format(line))
 
-            # 在数据库中修改打印状态为2，表示已经打印
-            printed_order = session.query(Order).filter(Order.File_Dir == line)
-            printed_order.Print_Status = 2
-            session.commit()
-            session.close()
+            # 在数据库中修改打印状态为3，表示已经打印
+            printed_order = session2.query(Order).filter(Order.File_Dir == line)
+            printed_order.Print_Status = 3
+            session2.commit()
+            # session2.close()
 
             # 在./log/print_access_log 中写入打印成功日志
             with open('./log/print_access_log', 'a') as f:
@@ -87,7 +82,7 @@ def Print():
 while 1:
     Query()
     time.sleep(5)
-    # Print()
+    Print()
 
 
 
