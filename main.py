93c730549ab3f333ef7209f3575e32d539b9942f
hopeaktian <hopeaktian@gmail.com>
2018-10-16 20:50:33 +0800

    debug much.

--- main.py
+++ main.py
@@ -9,35 +9,50 @@ description:
 
 from config import engine, Session
 from model import Base, Order
-import datetime, os, traceback
+import datetime, traceback, time, subprocess
 
 
-Base.metadata.create_all(engine)
-session = Session()
+
 
 def Query():
     # 查询排队方式为自动排队的订单
+    Base.metadata.create_all(engine)
+    session = Session()
     All_Order = session.query(Order).filter(Order.Time_Way == 1).all()
-    for i in range(len(All_Order)):
-        if All_Order[i].Print_Status == 1:
-            # 尝试下载订单中的文件
-            try:
-                os.system('wget http://rooins.careyou.xin/static/Upload_Files/{} -P ./User_Files/To_Print' .format(All_Order[i].File_Dir))
-            except Exception:
-                # 将错误写入下载错误日志
-                s = traceback.format_exc()
-                with open('./log/download_error_log') as f:
-                    f.write(str(datetime.datetime.now()) + " " + s + "\n")
-            finally:
-                # 将下载成功写入下载成功日志
-                with open("./log/download_log", "w") as f:
-                    f.write(str(datetime.datetime.now()) + " " + All_Order.File_Dir + "\n")
+    if All_Order:
+        for i in range(len(All_Order)):
+            if All_Order[i].Print_Status == 1:
+                # 尝试下载订单中的文件
+                try:
+                    cmd = "wget http://rooins.careyou.xin/static/Upload_Files/{} -P ./User_Files/To_Print" .format(All_Order[i].File_Dir)
+                    subprocess.call(cmd)
+                except Exception as e:
+                    # 将错误写入下载错误日志
+                    print "Error"
+                    with open('./log/download_error_log', 'a') as f:
+                        f.write(str(datetime.datetime.now()) + " " + All_Order[i].File_Dir + " " + str(e) + "\n")
+                else:
+                    # 将下载成功写入下载成功日志
+                    with open("./log/download_log", "a") as f:
+                        f.write(str(datetime.datetime.now()) + " " + All_Order[i].File_Dir + " " + "success-download" + "\n")
+                    # 在数据库中做出标记，文件已下载成功
+                    All_Order[i].Print_Status = 2
+                    print "Ok"
+                finally:
+                    session.commit()
+                    # session.close()
+                    # session.flush()
+                    print 'close'
+    print 'finished'
 
 def Print():
 
+    Base.metadata.create_all(engine)
+    session = Session()
     # 将 ./User_Files/To_Print/ 中的文件名导入到预打印日志中
-    os.system('ls ./User_Files/To_Print/ > ./log/ToPrint_filename')
-    ToPrint = open("./log/ToPrint_filename")
+    cmd = "ls ./User_Files/To_Print/ > ./log/ToPrint_filename"
+    subprocess.call(cmd)
+    ToPrint = open("./log/ToPrint_filename", 'r+')
     for line in ToPrint:
         try:
             # 开始尝试打印
@@ -45,7 +60,7 @@ def Print():
         except Exception:
             # 捕获错误，并将错误写入错误日志中
             s = traceback.format_exc()
-            with open('./log/print_error_log') as f:
+            with open('./log/print_error_log', 'a') as f:
                 f.write(str(datetime.datetime.now()) + " " + s + "\n")
         finally:
             # 将打印成功的文件移动到 ./User_Files/Finished_Print 这个目录中
@@ -58,12 +73,13 @@ def Print():
             session.close()
 
             # 在./log/print_access_log 中写入打印成功日志
-            with open('./log/print_access_log') as f:
-                f.write(str(datetime.datetime.now()) + " " + line + "success")
+            with open('./log/print_access_log', 'a') as f:
+                f.write(str(datetime.datetime.now()) + " " + line + "success-printed")
 
 while 1:
     Query()
-    Print()
+    time.sleep(5)
+    # Print()
 
 
 
