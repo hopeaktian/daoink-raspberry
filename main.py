6b8ba61598ad81037e5009b3d0466b5f657a6e63
hopeaktian <hopeaktian@gmail.com>
2018-10-18 20:32:44 +0800

    debug print successfully

--- main.py
+++ main.py
@@ -53,17 +53,14 @@ def Print():
     subprocess.call(cmd, shell=True)
     ToPrint = open("./log/ToPrint_filename", 'r+')
     for line in ToPrint:
-        print line
         try:
             # 开始尝试打印
-            print_cmd = 'lpr ./User_Files/To_Print/'+line
+            print_cmd = 'lpr ./User_Files/To_Print/'+line[:-1]
             returnCode = subprocess.call(print_cmd, shell=True)
             if returnCode != 0:
-                print commands.getoutput(print_cmd)
                 error = commands.getoutput(print_cmd)
                 raise IOError(error)
         except Exception as e:
-            print str(e)
             # 捕获错误，并将错误写入错误日志中
             with open('./log/print_error_log', 'a') as f:
                 f.write(str(datetime.datetime.now()) + " " + line[:-1] + " " + str(e) + "\n")
