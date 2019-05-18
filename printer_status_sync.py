88ec884c1475c47ac08c0f68bdd2c2250bf2fa3d
hopeaktian <hopeaktian@gmail.com>
2019-05-18 12:17:07 +0800

    update

--- printer_status_sync.py
+++ printer_status_sync.py
@@ -8,8 +8,12 @@ def status_sync(device_name):
     status = check_usb(device_name)
     check_time = int(time.time())
     r = redis.Redis(host=RedisConfig.host, password=RedisConfig.password, port=RedisConfig.port, db=0, decode_responses=True)
-    r.rpush("test_printer", status)
-    r.rpush("test_printer", check_time)
+    if r.exists('test_printer'):
+        r.lset("test_printer", 0, status)
+        r.lset("test_printer", 1, check_time)
+    else:
+        r.rpush("test_printer", status)
+        r.rpush("test_printer", check_time)
 
 while True:
     status_sync(Printer_grep)
